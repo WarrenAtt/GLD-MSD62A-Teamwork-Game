fafee447828cf3c7ccb7b17451a61df6c99e234e
WarrenAttard <warren.attard.e22454@mcast.edu.mt>
2022-12-07 10:42:39 +0100

    Enemy Animated On Waypoints

--- GLD-MSD62A-Teamwork-Game/Assets/Scripts/Enemy.cs
+++ GLD-MSD62A-Teamwork-Game/Assets/Scripts/Enemy.cs
@@ -1,16 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField]
     public int health = 10;
 
+    private List<GameObject> _waypoints;
+    private NavMeshAgent _agent;
+    private Animator _animator;
+    private Vector3 destination;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+        _waypoints = new List<GameObject>();
+
+        foreach (GameObject wp in GameObject.FindGameObjectsWithTag("Waypoint"))
+        {
+            _waypoints.Add(wp);
+        }
+
+        //destination = GameObject.FindGameObjectWithTag("Waypoint").transform.position;
+        MoveToNextDestination();
+
     }
 
     // Update is called once per frame
@@ -19,13 +37,54 @@ public class Enemy : MonoBehaviour
         if(health <= 0)
         {
             Destroy(this.gameObject);
-            print("Enemy Dead");
         }
+
+
+        if (_agent.remainingDistance <= 0.5f && _agent.isStopped == false)
+        {
+            _agent.isStopped = true;
+            _animator.SetBool("Walking", false);
+            StartCoroutine(WaitTimer(2, MoveToNextDestination));
+        }
+        
     }
 
     public void ReduceHealth()
     {
         health -= 1;
-        print("Damage Taken");
+    }
+
+    private void MoveToNextDestination()
+    {
+        List<GameObject> tempWaypoints = new List<GameObject>();
+
+        foreach(GameObject waypoint in _waypoints)
+        {
+            if(waypoint.transform.position != destination)
+            {
+                tempWaypoints.Add(waypoint);
+            }
+        }
+
+        if (_agent.remainingDistance <= 2f)
+        {
+
+            destination = tempWaypoints[UnityEngine.Random.Range(0, tempWaypoints.Count)].transform.position;
+        }
+
+        print(destination.ToString());
+
+        _animator.SetBool("Walking", true);
+        _agent.isStopped = false;
+
+        _agent.SetDestination(destination);
+
+
+    }
+
+    private IEnumerator WaitTimer(float time, Action callback)
+    {
+        yield return new WaitForSeconds(time);
+        callback();
     }
 }
